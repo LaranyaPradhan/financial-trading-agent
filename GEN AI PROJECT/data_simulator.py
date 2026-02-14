01b3681c6563505dbd76d871d72dd39e5d86d3eb
LaranyaPradhan <laranya011@gmail.com>
2026-02-15 01:24:46 +0530

    Delete GEN AI PROJECT directory

--- GEN AI PROJECT/data_simulator.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def generate_market_data(n=500):
-    np.random.seed(42)
-    prices = np.cumsum(np.random.randn(n)) + 100
-    return pd.DataFrame({"Close": prices})
